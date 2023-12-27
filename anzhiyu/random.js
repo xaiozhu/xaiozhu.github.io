ffc38c94878e807e0194bee4019813f5d81938ae
unknown <2026117756@qq.comgit config --global user.name xaiozhu>
2023-12-27 22:38:29 +0800

    Site updated: 2023-12-27 22:38:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/25/00-关于谈一场恋爱/","2023/12/24/hello-world - 副本/"];function toRandomPost(){
+var posts=["2023/12/27/阮报毒/","2023/12/24/hello-world - 副本/","2023/12/25/00-关于谈一场恋爱/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

c4dc4d0ed393fdcdea8ef8eb142ae822dcaea452
unknown <2026117756@qq.comgit config --global user.name xaiozhu>
2024-02-15 18:41:28 +0800

    Site updated: 2024-02-15 18:41:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/25/00-关于谈一场恋爱/","2024/01/20/《爱，喜欢，与被爱》超万字00后文学/","2023/12/24/hello-world - 副本/","2023/12/27/阮报毒/"];function toRandomPost(){
+var posts=["2023/12/27/阮报毒/","2023/12/24/hello-world - 副本/","2023/12/25/00-关于谈一场恋爱/","2024/01/20/《爱，喜欢，与被爱》超万字00后文学/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
